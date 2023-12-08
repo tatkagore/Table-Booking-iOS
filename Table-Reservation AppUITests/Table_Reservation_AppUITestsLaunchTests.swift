e3612b732704a5583043e1d1d681eb7e6c45d3e3
Tatiana Simmer <tatiana.simmer@qonto.com>
2023-12-08 16:06:32 +0100

    feat: tests init

---
+++ Table-Reservation AppUITests/Table_Reservation_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Table_Reservation_AppUITestsLaunchTests.swift
+//  Table-Reservation AppUITests
+//
+//  Created by Tatiana Simmer on 07/12/2023.
+//
+
+import XCTest
+
+final class Table_Reservation_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
